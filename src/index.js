457e591c7092238de74809e2216d571817553948
Mokhmad <mokhmad.tazbiev@gmail.com>
2022-01-28 11:48:44 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  completeTask,
+  titleChanged,
+  taskDeleted,
+  getTasks,
+} from './store/task';
+import configureStore from './store/store';
+
+const store = configureStore();
+
+const App = () => {
+  const [state, setState] = useState(store.getState());
+
+  useEffect(() => {
+    store.dispatch(getTasks());
+    store.subscribe(() => {
+      setState(store.getState());
+    });
+  }, []);
+
+  const changeTitle = (taskId) => {
+    store.dispatch(titleChanged(taskId));
+  };
+
+  const deleteTask = (taskId) => {
+    store.dispatch(taskDeleted(taskId));
+  };
+
+  return (
+    <div>
+      <h1>App</h1>
+      <ul>
+        {state.map((el) => (
+          <li key={el.id}>
+            <p>{el.title}</p>
+            <p>{el.description}</p>
+            <p>{`Completed ${el.completed}`}</p>
+            <button onClick={() => store.dispatch(completeTask(el.id))}>
+              complete
+            </button>
+            <button onClick={() => changeTitle(el.id)}>ChangeTitle</button>
+            <button onClick={() => deleteTask(el.id)}>Delete task</button>
+            <hr />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +56,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/store/store.js
@@ -0,0 +1,19 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import taskReducer from './task';
+import { logger } from './middleware/logger';
+import { thunk } from './middleware/thunk';
+
+const middlewareEnhancer = applyMiddleware(logger, thunk);
+
+function configureStore() {
+  return createStore(
+    taskReducer,
+    compose(
+      middlewareEnhancer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+}
+
+export default configureStore;
